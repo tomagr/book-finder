e651dce47b16872733d62547b71a5b5e9d794950
Tomas Agrimbau <tomas@amalgama.co>
2023-05-17 16:41:52 -0300

    Added fecthBooks method in useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./index.css";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
 
   const editBook = (id, title) => {
     const updatedBooks = books.map((book) => {
@@ -22,13 +32,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const handleCreateBook = (title) => {
-    let inventedId = Math.round(Math.random() * 9999);
-    const updatedBooks = [
-      ...books,
-      // { id: inventedId, title: title }
-      { id: inventedId, title }, // same as above
-    ];
+  const handleCreateBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
 
     setBooks(updatedBooks);
     console.log("updatedBooks", updatedBooks);
